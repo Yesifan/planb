d98b3f97ee105258cc51c498dc3ef99825c31b18
思凡 叶 <yesifan66@qq.com>
2026-04-13 10:49:33 +0800

    fix: drizzle config 和 db/index 使用同一个来源

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,33 @@
+import * as path from "path";
 import { defineConfig } from "drizzle-kit";
+
+const dbName = "planb.db";
+
+const currentDir = __dirname;
+const homeDir = process.env.HOME || process.env.USERPROFILE || "/tmp";
+const cacheDir = `${homeDir}/.cache/planb`;
+
+// Environment detection
+const NODE_ENV = process.env.NODE_ENV || "development";
+const IS_TEST = NODE_ENV === "test";
+const IS_PRODUCTION = NODE_ENV === "production";
+const DATABASE_URL = process.env.DATABASE_URL ?? path.join(cacheDir, dbName);
+const DEV_DATABASE_URL = path.join(
+  path.dirname(path.dirname(currentDir)),
+  dbName,
+);
+
+export const DB_PATH = IS_TEST
+  ? ":memory:"
+  : IS_PRODUCTION
+    ? DATABASE_URL
+    : DEV_DATABASE_URL;
 
 export default defineConfig({
   schema: "./lib/db/schema.ts",
   out: "./drizzle",
   dialect: "sqlite",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "./planb.db",
+    url: DB_PATH,
   },
 });
--- lib/db/index.ts
+++ lib/db/index.ts
@@ -8,29 +8,13 @@ import * as path from "path";
 import * as schema from "./schema";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import Database from "bun:sqlite";
+import { DB_PATH } from "@/drizzle.config";
 
 console.debug("NODE_ENV", process.env.NODE_ENV);
 
-const dbName = "planb.db";
-
-const currentDir = __dirname;
-
 // Environment detection
 const NODE_ENV = process.env.NODE_ENV || "development";
-const IS_TEST = NODE_ENV === "test";
 const IS_PRODUCTION = NODE_ENV === "production";
-const DATABASE_URL = process.env.DATABASE_URL;
-
-const DEV_DATABASE_URL = path.join(
-  path.dirname(path.dirname(currentDir)),
-  dbName,
-);
-
-const DB_PATH = IS_TEST
-  ? ":memory:"
-  : IS_PRODUCTION
-    ? (DATABASE_URL ?? DEV_DATABASE_URL)
-    : DEV_DATABASE_URL;
 
 if (IS_PRODUCTION) {
   const dir = path.dirname(DB_PATH);
