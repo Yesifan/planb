3e4fee3b6e986b28b05d98c25557d28d03988959
思凡 叶 <yesifan66@qq.com>
2026-04-20 15:49:19 +0800

    docs(chore): update docs, config, and refactor actions

--- app/api/auth/[...all].ts
+++
@@ -1,8 +0,0 @@
-import { toNodeHandler } from "better-auth/node";
-
-import { auth } from "@/lib/auth/server";
-
-// Disallow body parsing, we will parse it manually
-export const config = { api: { bodyParser: false } };
-
-export default toNodeHandler(auth.handler);
